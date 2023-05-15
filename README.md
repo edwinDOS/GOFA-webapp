97e032515c7f66630f32c146f722c6252782dc5a
Edwin Daniel Oña <eona@ing.uc3m.es>
2023-05-15 19:03:58 +0200

    Modified template with a table-like layout for creating buttons and swithes. Two buttons and swithes are available in this version, to control the real robot's I/O

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Steps to deploy the app in the real controller:
 1) Create the folder WebApps in the $HOME/ directory
 2) Copy/Paste the app folder in the WebApps folder (RW will detect the app)
 3) Restart the controller
+
+Attention: It is neccessary to check if the "Access Level" of the signals aimed to control is established to "All" instead of "Default"
+The "Access Level" parameter can be set up from RobotStudio Controller->Configuration->I/O System->Signal
--- app.js
+++ app.js
@@ -9,194 +9,131 @@ var myDigital;
 var mySwitch;
 var mySwitch2;
 var myIOData;
-var myStringData;
-var myBoolData;
-var myBoolDO1;
-var myBoolDO2;
+var myButton;
+var myButton2,di2,valueDI2;
 // change signalName to valid I/O signal name.
-var signalName = "Local_IO_0_DO3";
+var signal1;
+var signal2;
+var value;
+var value2;
+var di1;
+var valueDI1;
 
-// wait for HTML to finish loading before initiating any components.
+/*This procedure will be executed when the html page has finished loading.*/
 window.addEventListener("load", async function () {
+    fpComponentsEnableLog(); //Enables the console
+    createMainContent(); //Creates all components
+});
 
-    // uncomment line below to enable debug console
-    fpComponentsEnableLog();
+/* CONTENT MAIN  VIEW  */
+function createMainContent() {
+    Create_Switch1();
+    Create_Switch2();
+    //Create_Digital1();
+    Create_Button1();
+    Create_Button2();
+}
 
-    // wrap everything in a generic try catch in order to catch any potential error not already caught.
+
+function Create_Switch1() {
     try {
-        
-
         /* CREATE SWITCH COMPONENTS */
         mySwitch = new FPComponents.Switch_A();
         mySwitch.desc = "Local_IO_0_DO1";
-        mySwitch.attachToId("mySwitchDiv");
-        mySwitch.onchange = async function (active) {
+        mySwitch.attachToId("mySwitchDiv1");
+        mySwitch.onchange = async function (active) {            
             console.log(active);
-            myBoolData = myBoolDO1;
-            changeBoolData(myBoolData);
+            signal1 = await RWS.IO.getSignal('Local_IO_0_DO1');
+            value = await signal1.getValue();     
+            console.log(value);
+            Set_Pos1(value);
+            // if (value == 1) {
+            //     await RWS.IO.setSignalValue('Local_IO_0_DO1', 0);//The 'Changed' output is set to 0.
+            // } else {
+            //     await RWS.IO.setSignalValue('Local_IO_0_DO1', 1);//The 'Changed' output is set to 1.
+            // }
         }
+    } catch (e) { console.log("Something has gone wrong with the Up button!"); } //A popup is displayed if something has gone wrong
+}
 
+/* Function that updates the current position of the liftkit */
+async function Set_Pos1(value) {    
+    if (value == 1) {
+        await RWS.IO.setSignalValue('Local_IO_0_DO1', 0);//The 'Changed' output is set to 0.
+    } else {
+        await RWS.IO.setSignalValue('Local_IO_0_DO1', 1);//The 'Changed' output is set to 1.
+    }
+}
+
+
+function Create_Switch2() {
+    try{
         mySwitch2 = new FPComponents.Switch_A();
         mySwitch2.desc = "Local_IO_0_DO2";
-        mySwitch2.attachToId("myDO2Div");
-        mySwitch2.onchange = async function (active) {
+        mySwitch2.attachToId("mySwitchDiv2");
+        mySwitch2.onchange = async function (active) {            
             console.log(active);
-            myBoolData = myBoolDO2;
-            changeBoolData(myBoolData);
-        }
-
-        async function changeBoolData() {
-            let value = await myBoolData.getValue();
-            console.log("value is " + value);
-
-            try {
-                // we set the value to inverse of current value. If value is true, we set to false and vice versa.
-                await myBoolData.setValue(!value);
-            } catch (e) {
-                FPComponents.Popup_A.message(e.message, [e.httpStatus.code, e.controllerStatus.name, e.controllerStatus.description]);
+            signal2 = await RWS.IO.getSignal('Local_IO_0_DO2');
+            value2 = await signal2.getValue();   
+            console.log(value2);
+            if (value2 == 1) {
+                await RWS.IO.setSignalValue('Local_IO_0_DO2', 0);//The 'Changed' output is set to 0.
+            } else {
+                await RWS.IO.setSignalValue('Local_IO_0_DO2', 1);//The 'Changed' output is set to 1.
             }
         }
+    } catch (e) { console.log("Something has gone wrong with the Up button!"); } //A popup is displayed if something has gone wrong
+}
 
-        /* CREATE DIGITAL INDICATOR */
+/* function Create_Digital1() {
+    try{
         myDigital = new FPComponents.Digital_A();
-        myDigital.desc = "Local_IO_0_DO3";
-        myDigital.attachToId("myDigitalDiv");
-        myDigital.onclick = async function () {
-            var setValue = myDigital.active ? 0 : 1;
-            myDigital.active = !myDigital.active
-
-            try {
-                await myIOData.setValue(setValue);
-            } catch (e) {
-                FPComponents.Popup_A.message(e.message, [e.httpStatus.code, e.controllerStatus.name, e.controllerStatus.description]);
-            }
-
-        };
-
-        /* SUBSCRIBE TO IO SIGNAL */
-        try {
-            myIOData = await RWS.IO.getSignal(signalName);
-            myIOData.addCallbackOnChanged(async (newValue) => {
-                // first time this is called, newValue is undefined.
-                if (newValue == undefined) {
-                    newValue = await myIOData.getValue();
-                    console.log("value" + newValue);
-                }
-
-                // since 1 and 0 work for true or false we can use the new value directly to set UI status.
-                myDigital.active = newValue;
-                console.log("value" + newValue);
-            })
-            await myIOData.subscribe(true);
-            console.log("SUSCRIBE OK ");
-        } catch (e) {
-            FPComponents.Popup_A.message(e.message, [e.message, `Couldn't find I/O with name ${signalName}`]);
+        myDigital.attachToId("myLedDiv1");
+        myDigital.desc = "Local_IO_0_DI3";
+        myDigital.onclick = function () {
+            myDigital.active = !(myDigital.active);
         }
+    } catch (e) { console.log("Something has gone wrong with the Led button!"); } //A popup is displayed if something has gone wrong
+} */
 
-        /* SUBSCRIBE TO STRING BOOL VARIABLE */
-        myBoolDO1 = await RWS.Rapid.getData("T_ROB1", "Example", "isDO1");
-        myBoolDO1.addCallbackOnChanged(async function (value) {
-
-            var val = await myBoolDO1.getValue();
-            mySwitch.active = val;
-
-        });
-        await myBoolDO1.subscribe(true);
-
-        myBoolDO2 = await RWS.Rapid.getData("T_ROB1", "Example", "isDO2");
-        myBoolDO2.addCallbackOnChanged(async function (value) {
-
-            var val = await myBoolDO2.getValue();
-            mySwitch2.active = val;
-
-        });
-        await myBoolDO2.subscribe(true);
-
-    } catch (e) {
-        var err = JSON.stringify(e);
-        console.log(err);
-        console.log(e);
-        FPComponents.Popup_A.message("Something went wrong!", "Application might not work as intended");
-    }
-
-    this.initMenu();
-    this.initView();
-});
-
-var initMenu = function () {
-    var menu = new FPComponents.Menu_A();
-    menu.model = {
-        content: [
-            {
-                type: "gap"
-            },
-            {
-                type: "label",
-                label: "IO Example"
-            },
-            {
-                type: "gap"
+function Create_Button1(){
+    try{
+        myButton = new FPComponents.Button_A();
+        myButton.text = "Local_IO_0_DI1";
+        myButton.onclick = async function () {
+            di1 = await RWS.IO.getSignal('Local_IO_0_DI1');
+            valueDI1 = await di1.getValue();
+            //valueDI1 = !valueDI1;
+            console.log(valueDI1);
+            if (valueDI1 == 0){
+                await RWS.IO.setSignalValue('Local_IO_0_DI1', 1);//Invert value of DI1
+                myButton.highlight = true;
+            } else {
+                await RWS.IO.setSignalValue('Local_IO_0_DI1', 0);//Invert value of DI1
+                myButton.highlight = false;
             }
-        ]
-    }
-
-    menu.attachToId("menu-main");
+        };
+        myButton.attachToId("myButtonDiv1");
+    }catch (e) { console.log("Something has gone wrong with the Led button!"); } //A popup is displayed if something has gone wrong
 }
 
-var initView = function () {
-    var mainView = document.getElementById("main-view");
-    mainView.style.display = "flex";
-}
-
-// Here we should do things that should happen when app activate.
-var appActivate = async function () {
-    console.log("subscribe");
-    // subscribe to data
-    if (myBoolDO1) {
-        await myBoolDO1.subscribe(true);
-    }
-    if (myBoolDO2) {
-        await myBoolDO2.subscribe(true);
-    }
-
-    if (myStringData) {
-        await myStringData.subscribe(true);
-    }
-
-    if (myRapidData) {
-        await myRapidData.subscribe();
-    }
-
-    if (myIOData) {
-        await myIOData.subscribe(true);
-    }
-
-
-    return true;
-}
-
-// Here we should do things that should happen right before app deactivates.
-var appDeactivate = async function () {
-    console.log("unsubscribe");
-    // unsubscribe to data
-    if (myBoolDO1) {
-        await myBoolDO1.unsubscribe();
-    }
-    if (myBoolDO2) {
-        await myBoolDO2.unsubscribe();
-    }
-
-    if (myStringData) {
-        await myStringData.unsubscribe();
-    }
-
-    if (myRapidData) {
-        await myRapidData.unsubscribe();
-    }
-
-    if (myIOData) {
-        await myIOData.unsubscribe();
-    }
-
-    return true;
+function Create_Button2(){
+    try{
+        myButton2 = new FPComponents.Button_A();
+        myButton2.text = "Local_IO_0_DI2";
+        myButton2.onclick = async function () {
+            di2 = await RWS.IO.getSignal('Local_IO_0_DI2');
+            valueDI2 = await di2.getValue();
+            //valueDI1 = !valueDI1;
+            console.log(valueDI2);
+            if (valueDI2 == 0){
+                await RWS.IO.setSignalValue('Local_IO_0_DI2', 1);//Invert value of DI2
+                myButton2.highlight = true;
+            } else {
+                await RWS.IO.setSignalValue('Local_IO_0_DI2', 0);//Invert value of DI2
+                myButton2.highlight = false;
+            }
+        };
+        myButton2.attachToId("myButtonDiv2");
+    }catch (e) { console.log("Something has gone wrong with the Led button!"); } //A popup is displayed if something has gone wrong
 }
